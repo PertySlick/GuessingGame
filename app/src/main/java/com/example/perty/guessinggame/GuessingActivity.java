9a27d524f667f6a8d003f5073a9c5108afad5783
Tim Roush <perty_slick@hotmail.com>
2017-10-08 09:51:21 -0700

    Completed LandingActivity and initiated GuessingActivity

---
+++ app/src/main/java/com/example/perty/guessinggame/GuessingActivity.java
@@ -0,0 +1,8 @@
+package com.example.perty.guessinggame;
+
+/**
+ * Created by Perty on 10/8/2017.
+ */
+
+class GuessingActivity {
+}
--- app/src/main/java/com/example/perty/guessinggame/LandingActivity.java
+++ app/src/main/java/com/example/perty/guessinggame/LandingActivity.java
@@ -1,13 +1,50 @@
 package com.example.perty.guessinggame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LandingActivity extends AppCompatActivity {
+
+
+// VARIABLES CONSTANTS AND OBJECTS
+
+
+    Button submit;
+    TextView header;
+
+
+// METHODS
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        locateViews();
+        bindFunctionality();
+    }
+
+    // Identify necessary views
+    private void locateViews() {
+        submit = (Button) findViewById(R.id.submit_button);
+        header = (TextView) findViewById(R.id.header);
+    }
+
+    // Apply functionality to views
+    private void bindFunctionality() {
+        submit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GuessingActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
